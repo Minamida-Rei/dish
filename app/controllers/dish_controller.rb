6b3d5fad6035a14cff0400d5990202a62673082e
Minamida-Rei <rxnt1120@gmail.com>
2021-06-24 18:03:08 +0900

    献立出力機能の実装

--- app/controllers/dish_controller.rb
+++ app/controllers/dish_controller.rb
@@ -1,4 +1,23 @@
 class DishController < ApplicationController
   def index
+    @staple = Food.order("RAND()").first.staple
+    if @staple.blank?
+      return redirect_to dish_index_path
+    end
+
+    @main_dish = Food.order("RAND()").second.main_dish
+    if @main_dish.blank?
+      return redirect_to dish_index_path
+    end
+
+    @sub_dish = Food.order("RAND()").third.sub_dish
+    if @sub_dish.blank?
+      return redirect_to dish_index_path
+    end
+
+    @soup = Food.order("RAND()").fourth.soup
+    if @soup.blank?
+      return redirect_to dish_index_path
+    end
   end
 end
