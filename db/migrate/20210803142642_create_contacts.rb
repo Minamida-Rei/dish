5d23d22ef533ce8b5716b9d6518e0e8c76332dbf
Minamida-Rei <rxnt1120@gmail.com>
2021-08-03 23:28:21 +0900

    マイグレート

--- db/migrate/20210803142642_create_contacts.rb
+++ db/migrate/20210803142642_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
-
+      t.text :message
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_080415) do
+ActiveRecord::Schema.define(version: 2021_08_03_142642) do
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "foods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "staple"
